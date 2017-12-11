ba1d46748cf6da4ccf4cc79293f406154f98d692
lbaile200 <lbaile200@gmail.com>
2017-12-11 14:58:39 -0500

    scrub script for sharing

--- Personal/Python/test.py
+++ Personal/Python/test.py
@@ -4,14 +4,13 @@ import sys
 import csv
 import datetime
 import time
-import tweepy
 
 def test():
-#run speedtest-cli
+#python has it's own speedtest utility, so we use this.
     print 'running test'
     test = os.popen("python /home/cabbage/.local/lib/python2.7/site-packages/speedtest.py --simple").read()
     print 'ran'
-    #split the 3 line result (ping,down,up)
+    #Get the useful lines.
     lines = test.split('\n')
     print test
     ts = time.time()
@@ -27,7 +26,7 @@ def test():
         d = lines[1][10:14]
         u = lines[2][8:12]
     print date,p, d, u
-    #save the data to file for local network plotting
+    #save the data to file for local network plotting.  You'll want to change this for your own machine
     out_file = open('/home/cabbage/Documents/speeds.csv', 'a')
     writer = csv.writer(out_file)
     writer.writerow((ts*1000,p,d,u))
@@ -35,34 +34,4 @@ def test():
     test()
     print 'completed'
 
-    #con2twitter
-    def get_api():
-      auth = tweepy.OAuthHandler('9SAs3qCVxTAuckbnb95kCm2Mk', 'Ism3ukdFOob3D2X7eHBw0knSQcIdK0CaeiqZNUsSKOaIOMcUAt')
-      auth.set_access_token('605994263-sxgJAUHnb9wao2Kn0tru5huA9QipfbWQjLl153jx', 'XAmRKu6P7lUcY2hcVZKmguV1SQLeyOGQTIoPasgFVxMxg')
-      return tweepy.API(auth)
-
-    #try to tweet even if no connection
-    if "Cannot" in test:
-        try:
-            api = get_api()
-            tweet="Hey @Comcast @ComcastCares why is my internet down? I pay for 60down\\5up in Knoxville TN? #comcastoutage #comcast"
-            status = api.update_status(status=tweet)
-            #twit.statuses.update(status=tweet)
-        except:
-            pass
-
-    #tweet if down speed is too low
-    elif eval(d)<10:
-        print "trying to tweet"
-        try:
-            api = get_api()
-            tweet="Hey @Comcast why is my internet speed " + str(int(eval(d))) + "down\\" + str(int(eval(u))) + "up when I pay for 10down\\1up in Knoxville TN? @ComcastCares @xfinity #comcast #speedtest"
-            status = api.update_status(status=tweet)
-            #twit.statuses.update(status=tweet)
-        except Exception,e:
-            print str(e)
-            pass
-    return
-if __name__ == '__main__':
-    test()
-    print 'completed'
+test()
